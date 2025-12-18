16fd943ecf38d5cecbb90c6464e2163cfc5a7972
bala-amypo <bala@amypo.in>
2025-12-18 08:33:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/Validationentity.java
+++ src/main/java/com/example/demo/entity/Validationentity.java
@@ -57,7 +57,8 @@ public void setCreatedate(Date Createdate){
 }
 public void setPassword(String Password){
     this.Password=password;
-}
+} 
+public Valdationentity(long id,@NotNull@Size(min=2,max=10,message="must be 2 to 10 character"),String username,@Email())
 
 
 
