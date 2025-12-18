f4f8b4dc2a0881075bf08ad4cfa6931e5b361b64
bala-amypo <bala@amypo.in>
2025-12-18 08:23:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/Validationentity.java
+++ src/main/java/com/example/demo/entity/Validationentity.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GeneratedType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.size;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.
+import jakarta.validation.constraints;
 
 @Entity
 public class Validationentity{
@@ -22,7 +22,59 @@ private String Username;
 Private String  email;
 @NotNull(message="password is mandatory")
 private String password;
-@positive()
+@positive(message="Age must be positive")
+@Max(30)
 private int age;
+
+public Integer getId(){
+    return Id;
+}
+public String getName()
+{
+    return Name;
+}
+public String getEmail(){
+    return email;
+}
+public String getPassword(){
+    return password;
+}
+public Date getCreatedate(){
+    return createdate;
+}
+
+public void setName(String Name){
+    this.Name=Name;
+}
+public void setId(Integer Id){
+    this.Id=Id;
+}
+public void setEmail(String email){
+    this.email=email;
+}
+public void setCreatedate(Date Createdate){
+    this.createdate=createdate;
+}
+public void setPassword(String Password){
+    this.Password=password;
+}
+@Entity
+public class Validationentity{
+//id long//userName //email //password //age
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private long id;
+@NotNull
+@size(min=2,max=30,message="must be 2 to 10 character")
+private String Username;
+@Email(message="Email is not valid")
+Private String  email;
+@NotNull(message="password is mandatory")
+private String password;
+@positive(message="Age must be positive")
+@Max(30)
+private int age;
+
+
 
 }
