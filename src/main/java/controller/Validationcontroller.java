4614994d500167ab8c6b7d88f3c95163fe1bf722
bala-amypo <bala@amypo.in>
2025-12-18 08:58:24 +0000

    User commit

---
+++ src/main/java/controller/Validationcontroller.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+import  org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.Annotation.Autowired;
+import com.example.demo.service.Studentservice;
+import org.springframework.factory.Annotation.PostMapping;
+
+@Rest
+public class Validationcontroller{
+
+
+}
