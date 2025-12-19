1aea33f6a07f15c3e62a3683d0a871dbcdd45f6e
bala-amypo <bala@amypo.in>
2025-12-19 08:55:20 +0000

    User commit

--- src/main/java/controller/Timestampcontroller.java
+++ src/main/java/controller/Timestampcontroller.java
@@ -1,4 +1,8 @@
 import com.example.demo.entity.Timestamp;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestControler
