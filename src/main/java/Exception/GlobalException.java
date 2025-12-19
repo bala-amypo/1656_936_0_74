d444308431d0926410fcad8835b7d30226119bc1
bala-amypo <bala@amypo.in>
2025-12-19 06:13:13 +0000

    User commit

--- src/main/java/Exception/GlobalException.java
+++ src/main/java/Exception/GlobalException.java
@@ -2,7 +2,8 @@ package.com.example.demo.exception;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
+import org.springframework.web.bind.annotation.http.HttpStatus;
+import org.springframework.web.bind.annotation.
 @RestControllerAdvice
 public class GlobalException{
     @ExceptionHandler(FValidationException.class)
