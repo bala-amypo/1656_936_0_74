8845a4aa20ecdb846c55a879b69102bfb35547e9
bala-amypo <bala@amypo.in>
2025-12-19 06:04:13 +0000

    User commit

--- src/main/java/Exception/GlobalException.java
+++ src/main/java/Exception/GlobalException.java
@@ -1,5 +1,9 @@
 package.com.example.demo.exception;
 @RestControllerAdvice
 public class GlobalException{
-    @ExceptionHandler()
+    @ExceptionHandler(FValidationException.class)
+
+    public RespondEntity<String>handleValidationException(ValidationException ex){
+        return new ResponseEntity<String>
+    }
 }
