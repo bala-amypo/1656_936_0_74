82070471c990994335f0d70a15179b237520dad1
bala-amypo <bala@amypo.in>
2025-12-19 09:55:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studentcard.java
+++ src/main/java/com/example/demo/entity/Studentcard.java
@@ -5,4 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-@
+@Entity
+@Data
+@AllArgsconstructor
+@NoArgsconstructor
+public class studentcard{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    
+}
